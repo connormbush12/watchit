130108cfa198d5d8f2b2613af8510a3e19e4d6af
Connor Bush <connormbush12@gmail.com>
2020-11-01 15:09:26 -0600

    Testing console.logs

--- index-practice.js
+++ index-practice.js
@@ -9,7 +9,7 @@ program.argument('[filename]', 'Input filename here').action(async (args) => {
 	console.log(args.filename)
 	if(await fs.promises.access('.', args.filename) ) {
 		throw new Err
-		or('This is not a valid filename')
+		or('This is not a valid filename, homie')
 	}
 	console.log(args)
 })
